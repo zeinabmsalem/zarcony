8ca23e2660cea7344ad5cf491fc63a2f06ec77d8
Zeinab Salem <you@example.com>
2019-11-18 12:42:49 +0200

    adding orders options to home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,5 +19,32 @@
             </div>
         </div>
     </div>
+
+ <br/>
+
+    <div class="row">
+
+       <a href="/orders"><button class="btn btn-primary mb-2" style="margin-left:320px;">
+                List All Pizza  Orders</button></a><br/><br/>
+        
+        <a href="/newpizzas"><button class="btn btn-primary mb-2" style="margin-left: 200px;">Add Pizza</button></a>
+
+    </div>
+    <div class="row">
+
+            <form action="/search_order_byID_orbyName" method="get" style="margin-top: 50px;">
+                <label style="margin-left: 200px;">Order ID or Customer Name: </label>&nbsp;&nbsp;
+            <input type="text" name="search_order" placeholder="Serach by ID or Name">
+
+            <button class="btn btn-primary mb-2" style="margin-left: 400
+            px;">Retreive Order</button>
+
+            </form>
+  
+        <br/><br/>
+    </div>
+
 </div>
 @endsection
+
+
