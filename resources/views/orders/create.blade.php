7027031e5c4db6b0b4666cd618ea782a608c3038
Zeinab Salem <you@example.com>
2019-11-21 00:54:32 +0200

    adding some changes in the create view for not allowing same falvor and size twice

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -1,12 +1,20 @@
-@extends('layouts.app')
+<!DOCTYPE html>
 
-@if (Auth::check())
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@section('content')
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+ 
+  </head>
+  <body>
+    <br/>
 
-  <h1 style="margin-left: 100px;">Make Your Pizza Order</h1><br/>
-
+    <h1 style="margin-left: 100px;">Make Your Pizza Order</h1><br/>
 
 
 {{ Form::open(['route' => 'orders.store', 'method' => 'post']) }}
@@ -78,6 +86,15 @@
 {{ Form::close() }}
 
 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+    <script src="/assets/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
 <script>
   
 $('#save').click(function (e){
@@ -133,6 +150,7 @@ if({{ count($orders) }} > 0){
 </script>
 
 
-@endsection
+  </body>
+</html>
 
-@endif
+
